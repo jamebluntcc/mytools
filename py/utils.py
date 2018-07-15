279404c0c5fe2003dcbacd85143c1025eea57086
chencheng <chencheng@chenchengdeMacBook-Pro.local>
2018-07-15 16:12:08 +0800

    add transtime func

--- py/utils.py
+++ py/utils.py
@@ -1,11 +1,18 @@
 # coding: utf-8
 import re
+import time
+
 def isNumber(str):
     isNum = re.compile(r'^[-+]?[0-9]+\.[0-9]+$')
     if isNum.match(str) or str.isdigit():
         return True
     return False
 
+def transtime(arg, format="%Y-%m-%d %H:%M:%S"):
+    if isinstance(arg, int):
+        return time.strftime(format, time.localtime(arg))
+    elif isinstance(arg, str):
+        return int(time.mktime(time.strptime(arg, format)))
 
 if __name__ == '__main__':
     result = isNumber('120.00')
