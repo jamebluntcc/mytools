125738ebd436cfe8a98fa6aae17d29d276cc1405
chencheng <jamebluntxy@gmail.com>
2018-12-04 12:09:37 +0800

    add aws vpn

---
+++ sh/aws_vpn.sh
@@ -0,0 +1,10 @@
+sudo yum install -y python-setuptools
+sudo easy_install pip
+sudo pip install shadowsocks
+
+mkdir /etc/shadowsocks
+sudo vim /etc/shadowsocks/config.json
+
+sudo /bin/ssserver -c /etc/shadowsocks/config.json -d start # 启动
+sudo /bin/ssserver -c /etc/shadowsocks/config.json -d stop # 关闭
+sudo /bin/ssserver -c /etc/shadowsocks/config.json -d restart # 重启
