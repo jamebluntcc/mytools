6942084f7323b9d3259f1224fe906ed3a797f5c7
chencheng <jamebluntxy@gmail.com>
2018-12-14 10:39:16 +0800

    添加自定义异常

--- py/utils.py
+++ py/utils.py
@@ -2,11 +2,13 @@
 import re
 import time
 
+
 def isNumber(str):
     isNum = re.compile(r'^[-+]?[0-9]+\.[0-9]+$')
     if isNum.match(str) or str.isdigit():
         return True
     return False
+
 
 def transtime(arg, format="%Y-%m-%d %H:%M:%S"):
     if isinstance(arg, int):
@@ -14,6 +16,16 @@ def transtime(arg, format="%Y-%m-%d %H:%M:%S"):
     elif isinstance(arg, str):
         return int(time.mktime(time.strptime(arg, format)))
     return None
+
+
+class CustomError(Exception):
+    def __init__(self, errorInfo):
+        super(CustomError, self).__init__()
+        self.errorInfo = errorInfo
+
+    def __str__(self):
+        return self.errorInfo
+
 
 if __name__ == '__main__':
     result = isNumber('120.00')
